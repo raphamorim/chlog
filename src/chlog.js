e416a399f65cb38c5d6e539bfc9a37daf8cc703b
Raphael Amorim <rapha850@gmail.com>
2016-07-01 18:18:44 -0300

    log cause

--- src/chlog.js
+++ src/chlog.js
@@ -6,18 +6,21 @@ var packageJson = require('../package.json'),
     path = require('path'),
     utils = require('./utils');
 
-var chlog = new Function();
+var chlog = new Function(),
+    log = "";
 
 chlog.prototype.version = function() {
     return packageJson.version;
 }
 
 chlog.prototype.run = function() {
+    var gitRepo = null;
     return Git.Repository.open(path.resolve(__dirname, "../.git"))
         .then(function(repo) {
-            return repo.getCurrentBranch().then(function(ref) {
+            gitRepo = repo;
+            return gitRepo.getCurrentBranch().then(function(ref) {
                 // console.log("On " + ref.shorthand() + " (" + ref.target() + ")");
-                return repo.getBranchCommit(ref.shorthand());
+                return gitRepo.getBranchCommit(ref.shorthand());
             }).then(function(commit) {
                 /* Set up the event emitter and a promise to resolve when it finishes up. */
                 var hist = commit.history(),
@@ -31,13 +34,20 @@ chlog.prototype.run = function() {
                 for (var i = 0; i < commits.length; i++) {
                     var sha = commits[i].sha().substr(0, 7),
                         msg = commits[i].message().split('\n')[0];
-                    console.log("- " + utils.capitalizeFirstLetter(msg) + " (" + sha + ")");
+
+                    var action = "- " + utils.capitalizeFirstLetter(msg) + " (" + sha + ")";
+                    console.log(action);
+                    log = log + '\n' + action;
                 }
+
+                return Git.Tag.list(gitRepo).then(function(refs) {
+                    console.log(refs);
+                });
             });
         }).catch(function(err) {
             console.log(err);
         }).done(function() {
-            // console.log('Finished');
+            console.log(log);
         });
 }
 
